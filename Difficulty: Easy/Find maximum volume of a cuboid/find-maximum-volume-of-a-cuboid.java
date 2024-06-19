1b30251d1513d87f2359299f0b2c2e9efc85139d
AdityaJha2901 <92071927+AdityaJha2901@users.noreply.github.com>
2024-06-20 04:21:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find maximum volume of a cuboid/find-maximum-volume-of-a-cuboid.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+            String arr[] = read.readLine().trim().split("\\s+");
+            double perimeter = Double.parseDouble(arr[0]);
+            double area = Double.parseDouble(arr[1]);
+
+            Solution ob = new Solution();
+            double ans = ob.maxVolume(perimeter, area);
+            System.out.println(String.format("%.2f", ans));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+
+    double maxVolume(double p, double a) {
+        // code here
+       double l = (p - Math.sqrt((p * p) - (24 * a))) / 12;
+        double volume = l * l * (p / 4 - 2 * l);
+        return volume;
+    }
+}
